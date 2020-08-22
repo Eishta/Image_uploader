b1d80a10a1e75af18c19eb0cf4c766e05755e15a
Eishta <eishtamital@gmail.com>
2020-08-22 21:11:10 +0530

    Updates

---
+++ precache-manifest.d7a55f2ce8a0ff396a35fca3c7e458d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "362c136de81b2e4fadbc4e144f4df488",
+    "url": "/codeblock/index.html"
+  },
+  {
+    "revision": "5db7bd99132b9b10259f",
+    "url": "/codeblock/static/css/main.a11b7077.chunk.css"
+  },
+  {
+    "revision": "46806dabdf4a108d0477",
+    "url": "/codeblock/static/js/2.678703c8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/codeblock/static/js/2.678703c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5db7bd99132b9b10259f",
+    "url": "/codeblock/static/js/main.add2cd20.chunk.js"
+  },
+  {
+    "revision": "258fb6b41b32cbfe6c79",
+    "url": "/codeblock/static/js/runtime-main.833f1298.js"
+  }
+]);
